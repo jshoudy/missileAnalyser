203dcf5c2c69b82fbefed5e00a81bc28d3f41002
thecopy <thecopy@gmail.com>
2014-05-17 14:28:18 +0200

    Moved if testing to separate file

--- src/ExampleTest.java
+++ src/ExampleTest.java
@@ -1,84 +1,13 @@
 public class ExampleTest {
   public static void l(){
 	MissileBattery b = new MissileBattery(2);
-	b.fire(1);
-  }
-  public static void IF_TESTER() {
-	int i = 20;
-	int ii = 20;
-	MissileBattery b = new MissileBattery(2);
 	
-	int ten = 10;
-	// NEQ
-	if(20 != i){
-		b.fire(i);
-	}
-	if(ii != i){
-		b.fire(i);
-	}
-	if(i != 20){
-		b.fire(i);
-	}
 	
-	// LE  
-	if(i < 10){
-		b.fire(i);
+	int i = 4;
+	while(i >= 2){
+		i--;
 	}
-	if(i < ten){
-		b.fire(i);
-	}
-	if(10 < i){
-	}else{
-		b.fire(i);
-	}
-	
-	// LEQ
-	if(i <= 10){
-		b.fire(i);
-	}
-	if(i <= ten){
-		b.fire(i);
-	}
-	if(10 <= i){
-	}else{
-		b.fire(i);
-	}
-	
-	// EQ
-	if(i == 9){
-		b.fire(i);
-	}
-	if(i == ten){
-		b.fire(i);
-	}
-	if(9 == i){
-		b.fire(i); 
-	}
-
-	//GE
-	if(10 > i){
-		b.fire(i);
-	}
-	if(ten > i){
-		b.fire(i);
-	}
-	if(i > 30){
-		b.fire(i);
-	}
-
-	//GEQ
-	if(10 >= i){
-		b.fire(i);
-	}
-	if(ten >= i){
-		b.fire(i);
-	}
-	if(i >= 30){
-		b.fire(i);
-	}
-
-	b.fire(1);
-	
+	b.fire(i);
   }
 }
 
